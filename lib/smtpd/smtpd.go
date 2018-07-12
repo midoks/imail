a4ab08929b838d447e0ad9cbf66a221648d502f3
midoks <midoks@163.com>
2018-07-12 15:07:59 +0800

    up

--- lib/smtpd/smtpd.go
+++ lib/smtpd/smtpd.go
@@ -16,33 +16,41 @@ import (
 const (
 	CMD_READY           = iota
 	CMD_HELO            = iota
+	CMD_EHLO            = iota
 	CMD_AUTH_LOGIN      = iota
 	CMD_AUTH_LOGIN_USER = iota
 	CMD_AUTH_LOGIN_PWD  = iota
 	CMD_MAIL_FROM       = iota
 	CMD_RCPT_TO         = iota
 	CMD_DATA            = iota
+	CMD_DATA_END        = iota
 	CMD_QUIT            = iota
 )
 
 var stateList = map[int]string{
 	CMD_HELO:       "HELO",
+	CMD_EHLO:       "EHLO",
 	CMD_AUTH_LOGIN: "AUTH LOGIN",
-	CMD_MAIL_FROM:  "MAIL_FROM",
-	CMD_RCPT_TO:    "RCPT_TO",
+	CMD_MAIL_FROM:  "MAIL FROM",
+	CMD_RCPT_TO:    "RCPT TO",
 	CMD_DATA:       "DATA",
+	CMD_DATA_END:   ".",
 	CMD_QUIT:       "QUIT",
 }
 
 const (
 	MSG_INIT            = "220.init"
 	MSG_OK              = "220"
+	MSG_MAIL_OK         = "250"
 	MSG_BYE             = "221"
 	MSG_BAD_SYNTAX      = "500"
 	MSG_COMMAND_ERR     = "502"
 	MSG_COMMAND_TM_ERR  = "421"
 	MSG_AUTH_LOGIN_USER = "334.user"
 	MSG_AUTH_LOGIN_PWD  = "334.passwd"
+	MSG_AUTH_OK         = "235"
+	MSG_AUTH_FAIL       = "535"
+	MSG_DATA            = "354"
 )
 
 var msgList = map[string]string{
@@ -54,6 +62,10 @@ var msgList = map[string]string{
 	MSG_BAD_SYNTAX:      "Error: bad syntax",
 	MSG_AUTH_LOGIN_USER: "dXNlcm5hbWU6",
 	MSG_AUTH_LOGIN_PWD:  "UGFzc3dvcmQ6",
+	MSG_AUTH_OK:         "Authentication successful",
+	MSG_AUTH_FAIL:       "Error: authentication failed",
+	MSG_MAIL_OK:         "Mail OK",
+	MSG_DATA:            "End data with <CR><LF>.<CR><LF>",
 }
 
 var GO_EOL = getGoEol()
@@ -67,6 +79,7 @@ func getGoEol() string {
 
 type smtpdServer struct {
 	conn      net.Conn
+	connClose bool
 	state     int
 	startTime time.Time
 	errCount  int
@@ -158,22 +171,53 @@ func (this *smtpdServer) cmdAuthLogin(input string) bool {
 
 func (this *smtpdServer) cmdAuthLoginUser(input string) bool {
 	this.setState(CMD_AUTH_LOGIN_USER)
-	this.write(MSG_AUTH_LOGIN_USER)
+	this.write(MSG_AUTH_LOGIN_PWD)
 	return true
 }
 
 func (this *smtpdServer) cmdAuthLoginPwd(input string) bool {
 
 	this.setState(CMD_AUTH_LOGIN_PWD)
-	this.write(MSG_AUTH_LOGIN_PWD)
+	this.write(MSG_AUTH_OK)
 	return true
 }
 
 func (this *smtpdServer) cmdMailFrom(input string) bool {
-
-	if this.cmdCompare(input, CMD_MAIL_FROM) {
+	inputN := strings.SplitN(input, ":", 2)
+	if this.cmdCompare(inputN[0], CMD_MAIL_FROM) {
 		this.setState(CMD_MAIL_FROM)
-		this.write(MSG_AUTH_LOGIN_USER)
+		this.write(MSG_MAIL_OK)
+		return true
+	}
+	this.write(MSG_BAD_SYNTAX)
+	return false
+}
+
+func (this *smtpdServer) cmdRcptTo(input string) bool {
+	inputN := strings.SplitN(input, ":", 2)
+	if this.cmdCompare(inputN[0], CMD_RCPT_TO) {
+		this.setState(CMD_RCPT_TO)
+		this.write(MSG_MAIL_OK)
+		return true
+	}
+	this.write(MSG_BAD_SYNTAX)
+	return false
+}
+
+func (this *smtpdServer) cmdData(input string) bool {
+	if this.cmdCompare(input, CMD_DATA) {
+		this.setState(CMD_DATA)
+		this.write(MSG_DATA)
+		return true
+	}
+	this.write(MSG_BAD_SYNTAX)
+	return false
+}
+
+func (this *smtpdServer) cmdDataEnd(input string) bool {
+	if this.cmdCompare(input, CMD_DATA_END) {
+		this.setState(CMD_DATA_END)
+		this.write(MSG_DATA)
 		return true
 	}
 	this.write(MSG_BAD_SYNTAX)
@@ -184,6 +228,7 @@ func (this *smtpdServer) cmdQuit(input string) bool {
 	if this.cmdCompare(input, CMD_QUIT) {
 		this.write(MSG_BYE)
 		this.close()
+		this.connClose = true
 		return true
 	}
 	return false
@@ -192,6 +237,11 @@ func (this *smtpdServer) cmdQuit(input string) bool {
 func (this *smtpdServer) handle() {
 
 	for {
+
+		if this.connClose {
+			break
+		}
+
 		state := this.state
 		cmd, err := this.getString()
 		if err != nil {
@@ -211,29 +261,39 @@ func (this *smtpdServer) handle() {
 			if this.cmdAuthLogin(cmd) {
 				continue
 			}
-
 		} else if CMD_AUTH_LOGIN == state {
+
 			if this.cmdAuthLoginUser(cmd) {
 				continue
 			}
 		} else if CMD_AUTH_LOGIN_USER == state {
+
 			if this.cmdAuthLoginPwd(cmd) {
 				continue
 			}
 		} else if CMD_AUTH_LOGIN_PWD == state {
 
+			if this.cmdMailFrom(cmd) {
+				continue
+			}
 		} else if CMD_MAIL_FROM == state {
-
+			if this.cmdRcptTo(cmd) {
+				continue
+			}
 		} else if CMD_RCPT_TO == state {
-
+			if this.cmdData(cmd) {
+				continue
+			}
 		} else if CMD_DATA == state {
-
+			if this.cmdDataEnd(cmd) {
+				continue
+			}
 		} else {
 			this.write(MSG_COMMAND_ERR)
 		}
 
 		if this.cmdQuit(cmd) {
-			break
+			continue
 		}
 	}
 }
@@ -241,6 +301,7 @@ func (this *smtpdServer) handle() {
 func (this *smtpdServer) start(conn net.Conn) {
 	this.conn = conn
 	this.startTime = time.Now()
+	this.connClose = false
 	this.setState(CMD_READY)
 	this.write(MSG_INIT)
 	this.handle()
