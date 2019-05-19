d3630787cf785839615c99e2116cccbefbb4c795
midoks <midoks@163.com>
2019-05-19 21:49:35 +0800

    Update smtpd.go

--- lib/smtpd/smtpd.go
+++ lib/smtpd/smtpd.go
@@ -6,7 +6,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
 	"runtime"
+	"runtime/debug"
+	"runtime/trace"
 	"strings"
 	"time"
 )
@@ -344,6 +349,8 @@ func Start() {
 	}
 	defer ln.Close()
 
+	go pprof()
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -353,4 +360,50 @@ func Start() {
 		srv := SmtpdServer{}
 		go srv.start(conn)
 	}
+}
+
+//手动GC
+func gc(w http.ResponseWriter, r *http.Request) {
+	runtime.GC()
+	w.Write([]byte("StartGC"))
+}
+
+//运行trace
+func traceStart(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte("TrancStart"))
+	fmt.Println("StartTrancs")
+}
+
+//停止trace
+func traceStop(w http.ResponseWriter, r *http.Request) {
+	trace.Stop()
+	w.Write([]byte("TrancStop"))
+	fmt.Println("StopTrancs")
+}
+
+// go tool trace trace.out
+
+//运行pprof分析器
+func pprof() {
+	go func() {
+		//关闭GC
+		debug.SetGCPercent(-1)
+		//运行trace
+		http.HandleFunc("/start", traceStart)
+		//停止trace
+		http.HandleFunc("/stop", traceStop)
+		//手动GC
+		http.HandleFunc("/gc", gc)
+		//网站开始监听
+		http.ListenAndServe(":6060", nil)
+	}()
 }
