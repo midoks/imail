8eb23c2c4cabece4cc345877d50cabd1abc05b2c
midoks <midoks@163.com>
2019-05-29 21:24:52 +0800

    Update user.go

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -2,13 +2,19 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	// "github.com/dgrijalva/jwt-go/request"
 	"github.com/midoks/imail/app/models"
+	"time"
 )
 
 // "github.com/astaxie/beego"
 // "strconv"
 // "strings"
-// "time"
+
+const (
+	SecretKey = "imail"
+)
 
 //UserController ...
 type UserController struct {
@@ -17,8 +23,28 @@ type UserController struct {
 
 //Login ...
 func (t *UserController) In() {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := make(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
+	claims["iat"] = time.Now().Unix()
+	claims["id"] = "1"
+	claims["username"] = "midoks"
 
-	t.retOk("ok")
+	token.Claims = claims
+
+	tokenString, err := token.SignedString([]byte(SecretKey))
+	if err != nil {
+
+		fmt.Println("Error extracting the key")
+		// fatal(err)
+	}
+
+	// fmt.Println(tokenString)
+
+	// claims2, err := jwt.Parse(tokenString)
+	// fmt.Println(claims2)
+
+	t.retOk(tokenString)
 }
 
 //Login ...
@@ -28,7 +54,7 @@ func (t *UserController) Out() {
 
 func (t *UserController) Info() {
 	id, _ := t.GetInt("id")
-	fmt.Println(id)
+	// fmt.Println(id)
 	row, _ := models.UserGetById(id)
 	fmt.Println(row)
 	t.retOk(row)
