fff3294b53d4464336ae38cda6a465f476c90e26
midoks <midoks@163.com>
2019-05-29 22:14:07 +0800

    Update user.go

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	// "github.com/astaxie/beego"
 	"github.com/dgrijalva/jwt-go"
 	// "github.com/dgrijalva/jwt-go/request"
 	"github.com/midoks/imail/app/models"
@@ -41,8 +42,15 @@ func (t *UserController) In() {
 
 	// fmt.Println(tokenString)
 
-	// claims2, err := jwt.Parse(tokenString)
-	// fmt.Println(claims2)
+	claims2, err := jwt.Parse(tokenString, func(*jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		fmt.Println("转换为jwt claims失败.", err)
+	}
+
+	fmt.Println(claims2)
 
 	t.retOk(tokenString)
 }
