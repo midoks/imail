01ed7e63b9469db60bf11feb88fb6b05445ed8e6
midoks <midoks@163.com>
2019-06-01 14:57:07 +0800

    Update user.go

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -3,15 +3,9 @@ package controllers
 import (
 	"fmt"
 	// "github.com/astaxie/beego"
-	"github.com/dgrijalva/jwt-go"
 	// "github.com/dgrijalva/jwt-go/request"
 	"github.com/midoks/imail/app/models"
-	"time"
 )
-
-// "github.com/astaxie/beego"
-// "strconv"
-// "strings"
 
 //UserController ...
 type UserController struct {
@@ -24,21 +18,7 @@ func (t *UserController) In() {
 	username := t.GetString("username")
 	fmt.Println(username)
 
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := make(jwt.MapClaims)
-	claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
-	claims["iat"] = time.Now().Unix()
-	claims["id"] = "1"
-	claims["username"] = "midoks"
-
-	token.Claims = claims
-
-	tokenString, err := token.SignedString([]byte(SecretKey))
-	if err != nil {
-
-		fmt.Println("Error extracting the key")
-		// fatal(err)
-	}
+	tokenString := t.makeJwt("d", username)
 
 	// fmt.Println(tokenString)
 
