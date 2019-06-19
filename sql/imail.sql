a766ecad1baff7f845cc6c33d6f7596be47fbfb7
Mr Chen <midoks@163.com>
2019-06-19 11:36:45 +0800

    Update imail.sql

--- sql/imail.sql
+++ sql/imail.sql
@@ -1,19 +1,21 @@
 SET NAMES utf8mb4;
+
+
 -- ----------------------------
 -- Table structure for im_mail
 -- ----------------------------
 DROP TABLE IF EXISTS `im_mail`;
 CREATE TABLE `im_mail` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `uid` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户ID',
   `mail_from` varchar(255) NOT NULL DEFAULT '' COMMENT '邮件来源',
   `mail_to` varchar(255) NOT NULL DEFAULT '' COMMENT '发送给谁',
-  `content` text NOT NULL COMMENT '邮件内容',
+  `content` longtext NOT NULL COMMENT '邮件内容',
   `size` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '内容大小',
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态',
   `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='邮件内容表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for im_user
@@ -38,11 +40,12 @@ CREATE TABLE `im_user_box` (
   `uid` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
   `mid` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '邮件ID',
   `size` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '邮件字节大小',
-  `type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '类型， 0:接收邮件;1:发送邮件',
+  `cid` bigint(20) NOT NULL DEFAULT '0' COMMENT '类型， 0:接收邮件;1:发送邮件',
+  `is_deleted` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除',
   `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   `update_time` bigint(20) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='用户BOX';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for im_class
@@ -57,7 +60,7 @@ CREATE TABLE `im_class` (
   `update_time` bigint(20) NOT NULL COMMENT '更新时间',
   `create_time` bigint(20) NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COMMENT='分类';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='分类';
 
 -- ----------------------------
 -- Records of im_class
