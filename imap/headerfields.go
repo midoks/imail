1e39e5087f5f7a323e4c6b10ea3142eb582df933
midoks <midoks@163.com>
2019-06-27 21:51:32 +0800

    Create headerfields.go

---
+++ imap/headerfields.go
@@ -0,0 +1,15 @@
+package imap
+
+import (
+	"net/textproto"
+)
+
+type HeaderFields struct {
+	b []byte // Raw header field, including whitespace
+	k string
+	v string
+}
+
+func newHeaderFields(k, v string, b []byte) *HeaderField {
+	return &HeaderField{k: textproto.CanonicalMIMEHeaderKey(k), v: v, b: b}
+}
