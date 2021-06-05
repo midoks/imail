b9eb573f05167a2def7b5e1fccea27b182134d1b
midoks <midoks@163.com>
2021-06-05 10:03:23 +0800

    Update smtpd.go

--- smtpd/smtpd.go
+++ smtpd/smtpd.go
@@ -230,7 +230,7 @@ func (this *SmtpdServer) cmdEhlo(input string) bool {
 			this.w("250-AUTH LOGIN PLAIN\r\n")
 			this.w("250-AUTH=LOGIN PLAIN\r\n")
 			this.w("250-coremail 1Uxr2xKj7kG0xkI17xGrU7I0s8FY2U3Uj8Cz28x1UUUUU7Ic2I0Y2UFRbmXhUCa0xDrUUUUj\r\n")
-			this.w("250-STARTTLS\r\n")
+			//this.w("250-STARTTLS\r\n")
 			this.w("250-SIZE 73400320\r\n")
 			this.w("250 8BITMIME\r\n")
 			return true
