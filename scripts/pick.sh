083968e31d2be44b6e4ca00eb7cd6efdcae543aa
midoks <midoks@163.com>
2021-06-27 00:34:08 +0800

    up

--- scripts/pick.sh
+++ scripts/pick.sh
@@ -4,5 +4,19 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
+
+
+mkdir -p $curPath/tmp
+mkdir -p $curPath/package
+
+
 export CGO_ENABLED=0 GOOS=linux GOARCH=amd64
 cd $rootPath && go build imail.go
+
+cp $rootPath/imail $curPath/tmp
+cp -r $rootPath/conf $curPath/tmp
+
+
+cd $curPath/tmp && zip -r -q -o imail-linux-amd64.zip  ./ && mv imail-linux-amd64.zip $curPath/package
+
+
