8e208f900fbaaca68ea30e501f366e1f351c0821
midoks <midoks@163.com>
2021-09-03 19:05:33 +0800

    Update make.sh

--- scripts/make.sh
+++ scripts/make.sh
@@ -4,12 +4,12 @@ _os=`uname`
 _path=`pwd`
 _dir=`dirname $_path`
 
-sed "s:{APP_PATH}:${_dir}:g" $_dir/script/init.d/imail.tpl > $_dir/script/init.d/imail
-chmod +x $_dir/script/init.d/imail
+sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/imail.tpl > $_dir/script/init.d/imail
+chmod +x $_dir/scripts/init.d/imail
 
 
 if [ -d /etc/init.d ];then
-	cp $_dir/script/init.d/imail /etc/init.d/imail
+	cp $_dir/scripts/init.d/imail /etc/init.d/imail
 	chmod +x /etc/init.d/imail
 fi
 
