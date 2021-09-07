7217854ebeaf6ae7db8993ecd6931450a6b5b7ae
midoks <midoks@163.com>
2021-09-07 08:48:27 +0800

    Update task.go

--- internal/task/task.go
+++ internal/task/task.go
@@ -2,21 +2,32 @@ package task
 
 import (
 	"fmt"
+	"github.com/midoks/imail/internal/config"
 	"github.com/midoks/imail/internal/db"
 	"github.com/midoks/imail/internal/smtpd"
 	"github.com/robfig/cron"
+	"sync"
 	"time"
 )
 
 func TaskQueueeSendMail() {
+	var wg sync.WaitGroup
 	result := db.MailSendListForStatus(1)
-	// fmt.Println(result)
-	for _, val := range result {
+
+	for i, val := range result {
+
+		wg.Add(i)
 		err := smtpd.Delivery("", val.MailFrom, val.MailTo, []byte(val.Content))
 		if err == nil {
 			db.MailSetStatusById(val.Id, 1)
+		} else {
+			domain := config.GetString("mail.domain", "xxx.com")
+			postmaster := fmt.Sprintf("postmaster@%s", domain)
+			err = smtpd.Delivery("", postmaster, val.MailTo, []byte("系统退信息"))
+			fmt.Println("ddd:", err)
 		}
 		fmt.Println("send mail:", err)
+		wg.Wait()
 	}
 
 }
@@ -25,8 +36,8 @@ func Init() {
 	c := cron.New()
 
 	c.AddFunc("*/5 * * * * * ", func() {
-		TaskQueueeSendMail()
 		fmt.Println(fmt.Sprintf("TaskQueueeSendMail! time:%d", time.Now().Unix()))
+		TaskQueueeSendMail()
 	})
 
 	c.Start()
