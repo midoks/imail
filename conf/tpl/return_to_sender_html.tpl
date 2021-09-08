b2d45819b8c2fd21ffad9b72e8c7c9c623aa7e24
midoks <midoks@163.com>
2021-09-08 16:13:27 +0800

    邮件模板

---
+++ conf/tpl/return_to_sender_html.tpl
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>{TITLE}</title>
+</head>
+<body>
+<p style="font-size:14px; font-family:verdana; margin:0 0 10px;
+line-height:22px;
+text-align:left">很抱歉您发送的邮件被退回，以下是该邮件的相关信息：</p>
+
+<table style="width:90%; border-collapse:collapse; margin:0 0 15px" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td style="width:120px; font-family:verdana; font-size:14px; font-weight:bold; text-align:center; background:#E0ECF9; border:1px solid #C1D9F3">被退回邮件</td>
+		<td style="border:1px solid #C1D9F3; font-family:verdana; padding:5px 15px; line-height:20px; font-size:14px;">主 题：jjj <br/>时 间：2021-09-08 15:12:45 </td>
+	</tr>
+</table>
+
+<table style="width:90%; border-collapse:collapse; margin:0 0 10px" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td colspan="2" style="font-size:14px;font-weight:bold;padding:5px 10px;border:1px solid #C1D9F3; background:#C1D9F3">无法发送到 {ERR_TO_MAIL}</td>
+	</tr>
+	<tr>
+		<td style="width:120px; font-size:14px;text-align:center; background:#EFF5FB; border:1px solid #C1D9F3">退信原因</td>
+		<td style="border:1px solid #C1D9F3; font-family:verdana; padding:5px 15px; line-height:20px; font-size:14px;"> 未知原因 <br /> {ERR_MSG} </td>
+	</tr>
+	<tr>
+		<td style="width:120px;font-size:14px;text-align:center; background:#EFF5FB; border:1px solid #C1D9F3">解决方案</td>
+		<td style="border:1px solid #C1D9F3; font-family:verdana; padding:5px 15px; line-height:20px; font-size:14px;"> 请联系您的收件人（{ERR_TO_MAIL}）或其所在服务商了解退信原因。</td></tr>
+
+</table>
+
+<p style="font-size:14px; font-family:verdana; margin:0 0 10px; line-height:22px; text-align:left">此外，您还可以 <a href="https://github.com/midoks/imail/wiki/%E9%80%80%E4%BF%A1" target="_blank">点击这里</a> 获取更多关于退信的帮助信息。</p>
+</body>
+</html>
--- internal/libs/mail_tpl.go
+++ internal/libs/mail_tpl.go
@@ -8,20 +8,35 @@ import (
 	"time"
 )
 
-func GetMailReturnToSender(from string, to string, subject string, msg string) (string, error) {
+func GetMailReturnToSender(to string, err_to_mail string, msg string) (string, error) {
+
+	domain := config.GetString("mail.domain", "xxx.com")
+	postmaster := fmt.Sprintf("postmaster@%s", domain)
+
+	sendTime := time.Now().Format("Mon, 02 Jan 2006 15:04:05 -0700 (MST)")
+	sendVersion := fmt.Sprintf("imail/%s", config.App.Version)
+	boundaryRand := RandString(20)
+
 	data, err := ioutil.ReadFile("conf/tpl/return_to_sender.tpl")
 	if err != nil {
 		return "", err
 	}
 
-	sendTime := time.Now().Format("Mon, 02 Jan 2006 15:04:05 -0700 (MST)")
-	sendVersion := fmt.Sprintf("imail/%s", config.App.Version)
+	dataHtml, err := ioutil.ReadFile("conf/tpl/return_to_sender_html.tpl")
+	if err != nil {
+		return "", err
+	}
 
-	content := strings.Replace(string(data), "{MAIL_FROM}", from, -1)
+	contentHtml := strings.Replace(string(dataHtml), "{TILTE}", "邮箱退信", -1)
+	contentHtml = strings.Replace(contentHtml, "{ERR_MSG}", msg, -1)
+	contentHtml = strings.Replace(contentHtml, "{ERR_TO_MAIL}", err_to_mail, -1)
+
+	content := strings.Replace(string(data), "{MAIL_FROM}", postmaster, -1)
 	content = strings.Replace(content, "{RCPT_TO}", to, -1)
-	content = strings.Replace(content, "{SUBJECT}", subject, -1)
+	content = strings.Replace(content, "{SUBJECT}", "系统退信", -1)
 	content = strings.Replace(content, "{TIME}", sendTime, -1)
 	content = strings.Replace(content, "{VERSION}", sendVersion, -1)
-	content = strings.Replace(content, "{CONTENT}", msg, -1)
+	content = strings.Replace(content, "{CONTENT}", Base64encode(contentHtml), -1)
+	content = strings.Replace(content, "{BOUNDARY_LINE}", boundaryRand, -1)
 	return content, nil
 }
