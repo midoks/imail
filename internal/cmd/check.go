c4a8df35e0ea384a42e9dabb92cb6b075d3fe3df
midoks <midoks@163.com>
2021-09-08 21:49:34 +0800

    Update check.go

--- internal/cmd/check.go
+++ internal/cmd/check.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
-	// "fmt"
+	"errors"
+	"fmt"
+	"github.com/midoks/imail/internal/config"
+	"github.com/midoks/imail/internal/log"
 	"github.com/urfave/cli"
+	"net"
+	"os"
+	"strings"
 )
 
 var Check = cli.Command{
@@ -16,6 +22,50 @@ var Check = cli.Command{
 }
 
 func doCheck(c *cli.Context) error {
+	confFile := c.String("config")
+	if confFile == "" {
+		confFile = "conf/app.conf"
+	}
+
+	if _, err := os.Stat(confFile); err != nil {
+		if os.IsNotExist(err) {
+			return errors.New("imail config is not exist!")
+		} else {
+			return err
+		}
+	}
+
+	err := config.Load(confFile)
+	if err != nil {
+		log.Infof("imail config file load err:%s", err)
+		return errors.New("imail config file load err")
+	}
+	domain := config.GetString("mail.domain", "xxx.com")
+	//mx
+	mx, _ := net.LookupMX(domain)
+	lenMx := len(mx)
+	if 0 == lenMx {
+		fmt.Println("mx check fail")
+	}
+
+	if strings.Contains(mx[0].Host, ".") {
+		fmt.Println("mx check done")
+	}
+
+	fmt.Println(mx[0].Host, mx[0].Pref)
+
+	doText, _ := net.LookupTXT(domain)
+	fmt.Println(doText)
+	fmt.Println(len(doText))
+
+	doTextDNS, _ := net.LookupCNAME(domain)
+	fmt.Println(doTextDNS)
+	fmt.Println(len(doTextDNS))
+
+	for d, txt := range doTextDNS {
+		fmt.Println("txt:", d, txt)
+	}
+
 	//smtp,pop3,imap check
 
 	//dkim check
