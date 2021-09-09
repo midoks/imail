9d1a5f79ada57ca93a9128b88cb3ae4625580a8d
midoks <midoks@163.com>
2021-09-09 08:52:49 +0800

    Update task.go

--- internal/task/task.go
+++ internal/task/task.go
@@ -2,12 +2,15 @@ package task
 
 import (
 	"fmt"
+	"github.com/Shopify/go-rspamd"
 	"github.com/midoks/imail/internal/config"
 	"github.com/midoks/imail/internal/db"
 	"github.com/midoks/imail/internal/libs"
 	"github.com/midoks/imail/internal/smtpd"
 	"github.com/robfig/cron"
 	// "sync"
+	"context"
+	"os"
 	// "time"
 )
 
@@ -33,12 +36,30 @@ func TaskQueueeSendMail() {
 	}
 }
 
+func TaskRspamdCheck() {
+	fmt.Println("TaskRspamdCheck")
+	client := rspamd.New("http://127.0.0.1:11334")
+	pong, err := client.Ping(context.Background())
+	fmt.Println(pong, err)
+
+	f, err := os.Open("/Users/midoks/Desktop/t.eml")
+	fmt.Println(f, err)
+
+	email := rspamd.NewEmailFromReader(f)
+	checkRes, _ := client.Check(context.Background(), email)
+	fmt.Println(checkRes)
+}
+
 func Init() {
 	c := cron.New()
 
 	c.AddFunc("*/5 * * * * * ", func() {
 		// fmt.Println(fmt.Sprintf("TaskQueueeSendMail! time:%d", time.Now().Unix()))
 		TaskQueueeSendMail()
+	})
+
+	c.AddFunc("*/1 * * * * * ", func() {
+		TaskRspamdCheck()
 	})
 
 	c.Start()
