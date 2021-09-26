e4ef8a797a3f15899610b569980cefa7ea619002
midoks <midoks@163.com>
2021-09-26 23:16:57 +0800

    覆盖率测试

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+curPath=`pwd`
+
 
 # init
 
@@ -9,4 +11,19 @@ fi
 go mod tidy
 go mod vendor
 
-goreleaser --snapshot --skip-publish --rm-dist
+
+# test cover
+cd $curPath/internal/imap
+go test -coverprofile=cov.out -coverpkg ./...
+go tool cover -html cov.out -o index.html
+
+cd $curPath/internal/pop3
+go test -coverprofile=cov.out -coverpkg ./...
+go tool cover -html cov.out -o index.html
+
+cd $curPath/internal/smtpd
+go test -coverprofile=cov.out -coverpkg ./...
+go tool cover -html cov.out -o index.html
+
+
+# goreleaser --snapshot --skip-publish --rm-dist
