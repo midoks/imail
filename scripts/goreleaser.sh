eec4b41c6b987e5b3aa63ffce23467b072095065
midoks <midoks@163.com>
2021-09-27 13:24:53 +0800

    Update goreleaser.sh

--- scripts/goreleaser.sh
+++ scripts/goreleaser.sh
@@ -10,6 +10,9 @@ cd  $rootPath/conf/ && rm -rf ./app.conf
 
 cd $rootPath
 
+# brew install FiloSottile/musl-cross/musl-cross
+# brew install mingw-w64
+
 #goreleaser --snapshot --skip-publish --rm-dist
 
 #git tag -a 0.0.5 -m "release 0.0.5"
