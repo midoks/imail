71908c59348ac2b9d9e39579bea1e3a067fd15ed
midoks <midoks@163.com>
2021-09-27 19:55:14 +0800

    Update pick.sh

--- scripts/pick.sh
+++ scripts/pick.sh
@@ -2,6 +2,14 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
+# https://github.com/FiloSottile/homebrew-musl-cross
+# brew install FiloSottile/musl-cross/musl-cross --with-i486 --with-x86_64 --with-aarch64 --with-arm
+
+# brew install mingw-w64
+
+# sudo port install mingw-w64
+
+VERSION=0.0.5
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
 
@@ -13,25 +21,66 @@ mkdir -p $rootPath/tmp/package
 
 
 build_app(){
-	echo "build_app" $1 $2
 
-	echo "export CGO_ENABLED=0 GOOS=$1 GOARCH=$2"
-	echo "cd $rootPath && go build imail.go"
-
-	export CGO_ENABLED=0 GOOS=$1 GOARCH=$2
-	# export CGO_ENABLED=0 GOOS=linux GOARCH=amd64
-
-	if [ $1 == "darwin" ];then
-		export CGO_ENABLED=1
+	if [ -f $rootPath/tmp/build/imail ]; then
+		rm -rf $rootPath/tmp/build/imail
+		rm -rf $rootPath/imail
 	fi
 
+	if [ -f $rootPath/tmp/build/imail.exe ]; then
+		rm -rf $rootPath/tmp/build/imail.exe
+		rm -rf $rootPath/imail.exe
+	fi
+
+	echo "build_app" $1 $2
+
+	echo "export CGO_ENABLED=1 GOOS=$1 GOARCH=$2"
+	echo "cd $rootPath && go build imail.go"
+
+	export CGO_ENABLED=1 GOOS=$1 GOARCH=$2
+	# export CGO_ENABLED=1 GOOS=linux GOARCH=amd64
+
+
 	if [ $1 == "windows" ];then
-		export CGO_ENABLED=0
-		cd $rootPath && go build imail.go
-	else
+		
+		if [ $2 == "amd64" ]; then
+			export CC=x86_64-w64-mingw32-gcc
+			export CXX=x86_64-w64-mingw32-g++
+		else
+			export CC=i686-w64-mingw32-gcc
+			export CXX=i686-w64-mingw32-g++
+		fi
+
+		cd $rootPath && go build -o imail.exe -ldflags "-H windowsgui" imail.go
+
 		# -ldflags="-s -w"
-		cd $rootPath && go build  imail.go
 		# cd $rootPath && go build imail.go && /usr/local/bin/strip imail
+	fi
+
+	if [ $1 == "linux" ]; then
+		export CC=x86_64-linux-musl-gcc
+		if [ $2 == "amd64" ]; then
+			export CC=x86_64-linux-musl-gcc
+
+		fi
+
+		if [ $2 == "386" ]; then
+			export CC=i486-linux-musl-gcc
+		fi
+
+		if [ $2 == "arm64" ]; then
+			export CC=aarch64-linux-musl-gcc
+		fi
+
+		if [ $2 == "arm" ]; then
+			export CC=arm-linux-musleabi-gcc
+		fi
+
+		cd $rootPath && go build -a imail.go
+	fi
+
+	if [ $1 == "darwin" ]; then
+		cd $rootPath && go build imail.go
 	fi
 	
 	
@@ -40,33 +89,35 @@ build_app(){
 	cd $rootPath/tmp/build/conf/ && rm -rf ./app.conf
 
 	cp -r $rootPath/scripts $rootPath/tmp/build
+	cp -r $rootPath/LICENSE $rootPath/tmp/build
+	cp -r $rootPath/public $rootPath/tmp/build
+	cp -r $rootPath/templates $rootPath/tmp/build
+	cp -r $rootPath/README.md $rootPath/tmp/build
+
 	cd $rootPath/tmp/build && xattr -c * && rm -rf ./*/.DS_Store && rm -rf ./*/*/.DS_Store
 
 
 	if [ $1 == "windows" ];then
 		cp $rootPath/imail.exe $rootPath/tmp/build
-		rm -rf $rootPath/tmp/build/imail
 	else
-		rm -rf $rootPath/imail.exe
-		rm -rf $rootPath/tmp/build/imail.exe
 		cp $rootPath/imail $rootPath/tmp/build
 	fi
 
 	# zip
-	#cd $rootPath/tmp/build && zip -r -q -o ${PACK_NAME}-$1-$2.zip  ./ && mv ${PACK_NAME}-$1-$2.zip $rootPath/tmp/package
+	#cd $rootPath/tmp/build && zip -r -q -o ${PACK_NAME}_${VERSION}_$1_$2.zip  ./ && mv ${PACK_NAME}_${VERSION}_$1_$2.zip $rootPath/tmp/package
 	# tar.gz
-	cd $rootPath/tmp/build && tar -zcvf ${PACK_NAME}-$1-$2.tar.gz ./ && mv ${PACK_NAME}-$1-$2.tar.gz $rootPath/tmp/package
+	cd $rootPath/tmp/build && tar -zcvf ${PACK_NAME}_${VERSION}_$1_$2.tar.gz ./ && mv ${PACK_NAME}_${VERSION}_$1_$2.tar.gz $rootPath/tmp/package
 	# bz
-	#cd $rootPath/tmp/build && tar -jcvf ${PACK_NAME}-$1-$2.tar.bz2 ./ && mv ${PACK_NAME}-$1-$2.tar.bz2 $rootPath/tmp/package
+	#cd $rootPath/tmp/build && tar -jcvf ${PACK_NAME}_${VERSION}_$1_$2.tar.bz2 ./ && mv ${PACK_NAME}_${VERSION}_$1_$2.tar.bz2 $rootPath/tmp/package
 }
 
 golist=`go tool dist list`
-
 echo $golist
 
 build_app linux amd64
-build_app linux 386
-build_app linux armv7
+# build_app linux 386
+# build_app linux arm64
+# build_app linux arm
 build_app darwin amd64
 build_app windows 386
 build_app windows amd64
