5285c3d9915c048fa10be886697d1625c4b226ab
midoks <midoks@163.com>
2021-10-10 00:09:40 +0800

    Update dkim.go

--- internal/tools/dkim/dkim.go
+++ internal/tools/dkim/dkim.go
@@ -32,6 +32,7 @@ func makeRsa() ([]byte, []byte, error) {
 
 func CheckDomainA(domain string) error {
 	findIp, err := net.LookupIP(domain)
+	fmt.Println(domain, findIp)
 	if err != nil {
 		return err
 	}
