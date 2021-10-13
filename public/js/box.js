042fbb56452a74c53f5c334fd430be176c143e7d
midoks <midoks@163.com>
2021-10-13 22:34:20 +0800

    up

--- public/js/box.js
+++ public/js/box.js
@@ -7,11 +7,10 @@ console.log("box init 1212 start13...")
 
 function selectAll(obj){
 
-	$('input[name=mail_select').attr("checked",obj.checked);
+	$('input[name=mail_select]').attr("checked",obj.checked);
 
-	var mailSelect = $('input[name=mail_select');
-	console.log($('input[name=mail_select'));
-	console.log(obj);
+	var mailSelect = $('input[name=mail_select]');
+	console.log($('input[name=mail_select]'));
 
 	getSelectVal()
 }
@@ -19,6 +18,11 @@ function selectAll(obj){
 
 function getSelectVal(){
 
-	console.log($('input[name=mail_select').val())
+	var ids = "";
+	$("input[name=mail_select]").each(function(){
+	   if(this.checked){
+	       console.log("id",$(this).val());
+	   }
+	});
 	
 }
