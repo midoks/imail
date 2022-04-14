ca7f328ec37de7dc195d4a836b31796ee2e9d9c5
midoks <midoks@163.com>
2022-04-14 16:11:42 +0800

    Update string.go

--- internal/tools/string.go
+++ internal/tools/string.go
@@ -189,10 +189,9 @@ func SizeFormat(size float64) string {
 }
 
 func RandString(len int) string {
-	r := rand.New(rand.NewSource(time.Now().Unix()))
 	bytes := make([]byte, len)
 	for i := 0; i < len; i++ {
-		b := r.Intn(26) + 65
+		b := rand.Intn(26) + 65
 		bytes[i] = byte(b)
 	}
 	return string(bytes)
