5ed3c7b92046d2e361c2b1dcf2a2943229fcf5f0
midoks <midoks@163.com>
2022-05-06 21:24:03 +0800

    Update pick.sh

--- scripts/pick.sh
+++ scripts/pick.sh
@@ -7,7 +7,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 # brew install mingw-w64
 # sudo port install mingw-w64
 
-VERSION=0.0.12
+VERSION=0.0.13
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
 
