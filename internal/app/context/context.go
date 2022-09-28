43d3839553c798fd50b321b5a16c79bbcc1c9448
midoks <midoks@163.com>
2022-09-28 22:27:14 +0800

    Update context.go

--- internal/app/context/context.go
+++ internal/app/context/context.go
@@ -193,6 +193,8 @@ func Contexter() macaron.Handler {
 
 		// Get user from session or header when possible
 		uid := c.Session.Get("uid")
+
+		fmt.Println("ready[uid]:", uid)
 		if uid != nil {
 			u, err := db.UserGetById(uid.(int64))
 			if err == nil {
