2683fc8c0af88741693c0e7a467522b470e54359
midoks <midoks@163.com>
2022-09-28 21:43:25 +0800

    Update auth.go

--- internal/app/router/user/auth.go
+++ internal/app/router/user/auth.go
@@ -134,6 +134,7 @@ func LoginPost(c *context.Context, f form.SignIn) {
 	redirectTo, _ := url.QueryUnescape(c.GetCookie("redirect_to"))
 	c.SetCookie("redirect_to", "", -1, conf.Web.Subpath)
 
+	fmt.Println("redirectTo:", redirectTo)
 	fmt.Println("tools.IsSameSiteURLPath(redirectTo):", tools.IsSameSiteURLPath(redirectTo))
 	if tools.IsSameSiteURLPath(redirectTo) {
 		c.Redirect(redirectTo)
