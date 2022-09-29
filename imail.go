cc8b7eb5640f7a1b365e95e740e8667630bc2904
midoks <midoks@163.com>
2022-09-29 21:40:52 +0800

    0.0.16

--- imail.go
+++ imail.go
@@ -13,7 +13,7 @@ import (
 	"github.com/midoks/imail/internal/tools/syscall"
 )
 
-const Version = "0.0.15"
+const Version = "0.0.16"
 const AppName = "imail"
 
 //go:embed templates
--- scripts/pick.sh
+++ scripts/pick.sh
@@ -7,7 +7,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 # brew install mingw-w64
 # sudo port install mingw-w64
 
-VERSION=0.0.15
+VERSION=0.0.16
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
 
